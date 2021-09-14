e91f229259f658ef72f4f018f74af9b7acbd2354
Charli Wild <charlie.wild@thinkmoney.co.uk>
2021-09-14 15:13:48 +0100

    complete class solution

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-function sum(a, b) {
-    return a + b;
+class Account {
+    constructor() {
+        this.balance = 0;
+        this.transactionHistory = [];
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+        this.transactionHistory.push({ "date": "14/09/2021", "amount": amount, "balance": this.balance });
+    }
+    withdraw(amount) {
+        this.balance -= amount;
+        this.transactionHistory.push({ "date": "14/09/2021", "amount": -amount, "balance": this.balance })
+    }
+    getBalance() {
+        return this.balance;
+    }
+    printTransactionHistory() {
+        let printOut = "Date || Amount || Balance\n"
+        this.transactionHistory.reverse().forEach((transaction) => {
+            printOut += `${transaction.date} || ${transaction.amount} || ${transaction.balance}\n`
+        })
+        return printOut;
+    }
 }
+
 module.exports = {
-    sum,
+    Account
 };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,34 @@
-const { sum } = require('./index.js');
+const { Account } = require('./index.js');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test('when we deposit 50, our balance is 50', () => {
+    let newAccount = new Account();
+    newAccount.deposit(50);
+    expect(newAccount.getBalance()).toEqual(50);
+})
+
+test('when we withdraw 50, our balance is 50', () => {
+    let newAccount = new Account();
+    newAccount.deposit(100);
+    newAccount.withdraw(50);
+    expect(newAccount.getBalance()).toEqual(50);
+})
+
+test('when we deposit 50, a new element gets added to the transaction history', () => {
+    let newAccount = new Account();
+    newAccount.deposit(1000);
+    expect(newAccount.transactionHistory[0]).toEqual({ "date": "14/09/2021", "amount": 1000, "balance": 1000 })
 });
+
+test('when we withdraw 500, a new element gets added to the transaction history', () => {
+    let newAccount = new Account();
+    newAccount.deposit(1000);
+    newAccount.withdraw(500);
+    expect(newAccount.transactionHistory[1]).toEqual({ "date": "14/09/2021", "amount": -500, "balance": 500 })
+});
+
+test('when we deposit 1000, and withdraw 500, we expect the transaction history to be printed', () => {
+    let newAccount = new Account();
+    newAccount.deposit(1000);
+    newAccount.withdraw(500);
+    expect(newAccount.printTransactionHistory()).toBe("Date || Amount || Balance\n14/09/2021 || -500 || 500\n14/09/2021 || 1000 || 1000\n")
+})
